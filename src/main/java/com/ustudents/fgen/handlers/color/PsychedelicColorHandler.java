27666f61bd63e69537b7082add970e50add8a2de
Hugo Kindel <hugokindel.pro@protonmail.com>
2021-12-19 18:49:23 +0100

    Add PsychedelicColorHandler

---
+++ src/main/java/com/ustudents/fgen/handlers/color/PsychedelicColorHandler.java
@@ -0,0 +1,11 @@
+package com.ustudents.fgen.handlers.color;
+
+import com.ustudents.fgen.common.json.JsonSerializable;
+
+@JsonSerializable(serializeClassName = true)
+public class PsychedelicColorHandler implements ColorHandler {
+    public int computeColor(int index, int maxIterations) {
+        int color = ((index % 256) << 16) | (((index + 85) % 256) << 8) | ((index + 170) % 256);
+        return color * 3 + color / 4;
+    }
+}
