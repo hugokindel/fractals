72f1e6a28458b11f0457fd29e0f0ff0555ff7f30
Hugo Kindel <hugokindel.pro@protonmail.com>
2021-12-19 18:56:49 +0100

    More Psycho

---
+++ src/main/java/com/ustudents/fgen/handlers/color/LightPsychedelicColorHandler.java
@@ -0,0 +1,13 @@
+package com.ustudents.fgen.handlers.color;
+
+import com.ustudents.fgen.common.json.JsonSerializable;
+
+@JsonSerializable(serializeClassName = true)
+public class LightPsychedelicColorHandler implements ColorHandler {
+    public int computeColor(int index, int maxIterations) {
+        int red = ((index % 256) << 16) * 3 + ((index % 256) << 16) / 4;
+        int green = (((index + 85) % 256) << 8) * 3 + (((index + 85) % 256) << 8) / 4;
+        int blue = ((index + 170) % 256) * 3 + ((index + 170) % 256) / 4;
+        return (red | green | blue) * 3 + (red | green | blue) / 4;
+    }
+}
