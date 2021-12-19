f0f8b097871594bc9105c31706ec95556cd95765
Hugo Kindel <hugokindel.pro@protonmail.com>
2021-12-19 20:51:58 +0100

    Mandelbrot qsd

--- src/main/java/com/ustudents/fgen/handlers/calculation/SimpleCalculationHandler.java
+++ src/main/java/com/ustudents/fgen/handlers/calculation/SimpleCalculationHandler.java
@@ -2,10 +2,7 @@ package com.ustudents.fgen.handlers.calculation;
 
 import com.ustudents.fgen.common.json.JsonSerializable;
 import com.ustudents.fgen.fractals.Fractal;
-import com.ustudents.fgen.maths.Complex;
 import com.ustudents.fgen.maths.ComplexPlane;
-
-import java.util.function.Function;
 
 @JsonSerializable(serializeClassName = true)
 public class SimpleCalculationHandler extends CalculationHandler {
@@ -25,9 +22,7 @@ public class SimpleCalculationHandler extends CalculationHandler {
 
         for (int y = 0 ; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Complex z0 = fractal.getZ0(plane, x, y, originX, originY, offsetX, offsetY);
-                Function<Complex, Complex> f = fractal.getF();
-                divergenceIndexes[y][x] = computeDivergenceIndex(z0, f);
+                divergenceIndexes[y][x] = computeDivergenceIndex(fractal.getZ0(plane, x, y, originX, originY, offsetX, offsetY), fractal.getF());
             }
         }
 
